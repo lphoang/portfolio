f43048c2c5240a727c76d941bc5769e1f3efe2e1
lphoang <lephuochoang98tcv@gmail.com>
2022-12-29 18:08:12 +0700

    Feat: delete redundant files

--- prettier.config.js
+++ prettier.config.js
@@ -1,14 +1,13 @@
-const options = {
-  importOrder: ['^components/(.*)$', '^[./]'],
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true,
-  arrowParens: 'avoid',
-  singleQuote: true,
-  bracketSpacing: true,
-  endOfLine: 'lf',
-  semi: false,
-  tabWidth: 2,
-  trailingComma: 'none'
+module.exports = {
+  "semi": false,
+  "trailingComma": "none",
+  "importOrder": ["^components/(.*)$", "^[./]"],
+  "importOrderSeparation": true,
+  "importOrderSortSpecifiers": true,
+  "arrowParens": "avoid",
+  "singleQuote": true,
+  "bracketSpacing": true,
+  "endOfLine": "lf",
+  "semi": false,
+  "tabWidth": 2,
 }
-
-module.exports = options
